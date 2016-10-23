8f663040a4e6cfcddb11487dda852162fe2c4174
Miguel Gasca <miguelgasca@gmail.com>
2016-10-23 13:55:13 +0200

    Using promises

--- layersToPng.js
+++ layersToPng.js
@@ -2,60 +2,70 @@ var PSD = require('psd');
 var exec = require('child_process').exec;
 var fs = require('fs-extra');
 var path = require('path');
+var RSVP = require('rsvp');
 
 var command = 'TexturePacker --data out/en/spt-game-{v}.json --format easeljs --sheet out/en/myTest-{v}-{n}.png --multipack --variant 1:hi --variant 0.5:low in/game/common/*.png in/game/en/*.png';
 var file = process.argv[2] || './sample.psd';
 
 var PATHS = {
-  png: 'png'
+    png: 'png'
 };
 
 function extractLayers(file) {
-  console.log(file);
-  console.log('Begin layer extract');
-  PSD.open(file).then(function (psd) {
-      console.log('psd found, begin extraction');
-      psd.tree().descendants().forEach(function (node) {
-          console.log('here');
-          if (node.isGroup()) {
-              return true;
-          }
+    var promises = [];
 
-          fs.ensureDir('png', function(err){
-            if(err) {
-              console.log(err);
-              return;
+    PSD.open(file).then(function (psd) {
+        console.log('psd found, begin extraction');
+        psd.tree().descendants().forEach(function (node) {
+            promises.push(layerToPng(node));
+        });
+        RSVP.all(promises).then(function (pngs) {
+            console.log('All DONE!');
+        });
+    });
+}
+
+function layerToPng(layer) {
+    var promise = new RSVP.Promise(function (resolve, reject) {
+        if (layer.isGroup()) {
+            return true;
+        }
+
+        fs.ensureDir(PATHS.png, function (err) {
+            if (err) {
+                console.log(err);
+                reject(err);
+                return;
             }
-            console.log(PATHS.png);
 
-            node.saveAsPng(path.join(PATHS.png, node.name + '.png'))
-            .then(function(e){
-              console.log('Extracted: ' + node.name + '.png');
-            })
-            .catch(function (err) {
-                console.log(err.stack);
+            layer.saveAsPng(path.join(PATHS.png, layer.name + '.png'))
+            .then(function (e) {
+                console.log('Extracted: ' + layer.name + '.png');
+                resolve(layer);
+            }).catch(function (err) {
+                console.log(err);
+                reject(err);
             });
-          });
-      });
-  });
+        });
+    });
+    return promise;
 }
 
 function start() {
-  console.log('start');
-  clean();
+    console.log('start');
+    clean();
 }
 
 function clean() {
-  console.log('Clean up');
-  fs.emptyDir(PATHS.png, function(err) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log('clean done!')
-    extractLayers(file);
-
-  });
+    console.log('Clean up');
+    fs.emptyDir(PATHS.png, function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('clean done!');
+        extractLayers(file);
+    });
 }
 
 start();
